6b017a74766d1b1dd919919da87733a792f10a43
Pravin Khabile <khabilepravin@gmail.com>
2019-01-10 07:07:12 +1100

    Further change for bootstrapping

---
+++ apiProxy/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using apiProxy.proxyGeneratedCode.API;
+using Autofac;
+
+namespace apiProxy
+{
+    public static class Bootstrapper
+    {
+        public static void Bootstrap(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<MediaApi>().As<IMediaApi>();
+        }
+    }
+}
