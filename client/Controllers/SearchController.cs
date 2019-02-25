434f574d30b6a3104861bf2a4732739c50ef01ff
Pravin Khabile <khabilepravin@gmail.com>
2019-02-25 22:05:11 +1100

    Minor search related changes

--- client/Controllers/SearchController.cs
+++ client/Controllers/SearchController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using apiProxy.impl;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace client.Controllers
 {
     public class SearchController : Controller
     {
+       private readonly ISearchApi _searchApi;
+        public SearchController(ISearchApi searchApi)
+        {
+            _searchApi = searchApi;
+                
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("{quotePartial}")]
+        public async Task<IActionResult> SearchQuotes([FromRoute]string quotePartial)
+        {
+            var response = await _searchApi.Search(quotePartial);
+
+            return View("results", response);
         }
     }
 }
