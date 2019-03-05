1809522956f523a53d1a75ec8b020430cd66111b
Pravin Khabile <khabilepravin@gmail.com>
2019-03-05 23:34:12 +1100

    Search media integration

--- client/wwwroot/js/main.js
+++ client/wwwroot/js/main.js
@@ -1,15 +1,18 @@
-﻿var app = new Vue({
+﻿var dataObject =  {
+    searchText: "",
+    items: [] = []
+};
+
+var app = new Vue({
     el: '#mainApp',
-    data: {
-        searchText:""
-    },
+    data: dataObject,
     methods: {
         search(event) {            
             if (event.code === 'Enter' || event.code === 'NumpadEnter') {
-                console.log(this.$data.searchText);
+               // console.log(this.data.searchText);
                 axios.get(`${baseUrl}/search/searchmedia?mediaText=${this.$data.searchText}`)
                     .then(function (response) {
-                        console.log(response.data);
+                        app.$data.items = response.data;
                     })
                     .catch(function (error) {
                         console.log(error.response);
