822392522c1425d151c68eb1f085875210aac2a8
Pravin Khabile <khabilepravin@gmail.com>
2019-04-11 22:13:03 +1000

    Furhter auth0 integration

---
+++ client/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace client.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("login")]
+        public async Task Login(string returnUrl = "/")
+        {
+            await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
--- client/Controllers/HomeController.cs
+++ client/Controllers/HomeController.cs
@@ -1,15 +1,44 @@
 ﻿using client.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace client.Controllers
 {
     public class HomeController : Controller
     {
         [Route("")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // If the user is authenticated, then this is how you can get the access_token and id_token
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                // if you need to check the access token expiration time, use this value
+                // provided on the authorization response and stored.
+                // do not attempt to inspect/decode the access token
+                DateTime accessTokenExpiresAt = DateTime.Parse(
+                    await HttpContext.GetTokenAsync("expires_at"),
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.RoundtripKind);
+
+                string idToken = await HttpContext.GetTokenAsync("id_token");
+
+                // Now you can use them. For more info on when and how to use the
+                // access_token and id_token, see https://auth0.com/docs/tokens
+            }
+
             return View();
+        }
+
+        [Route("callback")]
+        public IActionResult Callback()
+        {
+            return View("Callback");
         }
 
         public IActionResult About()
