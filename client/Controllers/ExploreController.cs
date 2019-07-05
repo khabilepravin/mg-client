f38c6cf99069f70cf3a7f57d3309f9c380ce27d0
Pravin Khabile <khabilepravin@gmail.com>
2019-07-05 21:54:49 +1000

    Changes to make a minimum viable code for staging

--- client/Controllers/ExploreController.cs
+++ client/Controllers/ExploreController.cs
@@ -4,6 +4,7 @@ namespace client.Controllers
 {
     public class ExploreController : Controller
     {
+        [Route("/explore/categories")]
         public IActionResult Categories()
         {
             return View();
