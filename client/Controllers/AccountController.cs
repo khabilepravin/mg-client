0d65d35fe81dd8ec2ed7c1354886d517eeaa9331
Pravin Khabile <pravin.khabile@leapdev.io>
2020-03-29 21:54:05 +1100

    Changes for login and logout states

--- client/Controllers/AccountController.cs
+++ client/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,6 +14,13 @@ namespace client.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
         [Route("/account/login")]
         public async Task Login(string returnUrl = "/")
         {
@@ -19,16 +28,20 @@ namespace client.Controllers
         }
 
         [Authorize]
+        [Route("/account/logout")]
         public async Task Logout()
         {
-            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
-            {
-                // Indicate here where Auth0 should redirect the user after a logout.
-                // Note that the resulting absolute Uri must be whitelisted in the
-                // **Allowed Logout URLs** settings for the client.
-                RedirectUri = Url.Action("Index", "Home")
-            });
+            // Sign the user out of the cookie authentication middleware (i.e. it will clear the local session cookie)
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Construct the post-logout URL (i.e. where we'll tell Auth0 to redirect after logging the user out)
+            var request = HttpContext.Request;
+            string postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + Url.Action("Index", "Home");
+
+            // Redirect to the Auth0 logout endpoint in order to log out of Auth0
+            string logoutUri = $"https://{_configuration["Auth0:Domain"]}/v2/logout?client_id={_configuration["Auth0:ClientId"]}&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+            HttpContext.Response.Redirect(logoutUri);
+            
         }
 
         [Authorize]
